a4318d0a0570dfba3e2398122d41c8168c2bb812
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-20 14:21:57 +0430

    make create user and find user fuctiion

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@nestjs/common';
-
-// This should be a real class/interface representing a user entity
-export type User = any;
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './users.dtos';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = this.userRepository.create(createUserDto);
+    return this.userRepository.save(newUser);
+  }
+
+  findUsersById(id: number) {
+    return this.userRepository.findOne({ where: { id } });
   }
 }
