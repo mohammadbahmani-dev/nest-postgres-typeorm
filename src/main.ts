09c3fe311e14bfea34106799633337df592d7fcb
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-24 10:14:34 +0430

    import and use sessions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'HDHFIDIFIOENVOINSFHNVFISONVSOINVIOSNYVDFGBDVD',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
