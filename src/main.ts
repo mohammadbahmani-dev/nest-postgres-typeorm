b13a6426e96cae01eb979301ebf4cfd73da937aa
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-25 14:29:07 +0430

    add session

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { TypeormStore } from 'connect-typeorm';
+import { getRepository } from 'typeorm';
+import { SessionEntity } from './typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const sessionRepository = getRepository(SessionEntity);
   app.use(
     session({
       name: 'nestjs session',
@@ -14,6 +18,7 @@ async function bootstrap() {
       cookie: {
         maxAge: 60000,
       },
+      store: new TypeormStore().connect(sessionRepository),
     }),
   );
   app.use(passport.initialize());
