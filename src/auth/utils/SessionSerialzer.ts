75181fee69f0f779078bd4311732bddaa34c591f
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-25 12:49:27 +0430

    add session serializer and deserializer

--- src/auth/utils/SessionSerialzer.ts
+++ src/auth/utils/SessionSerialzer.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
+import { User } from 'src/typeorm';
 import { UsersService } from 'src/users/users.service';
 
 export class sessionSerializer extends PassportSerializer {
@@ -9,6 +10,11 @@ export class sessionSerializer extends PassportSerializer {
     super();
   }
 
-  serializeUser() {}
-  deserializeUser() {}
+  serializeUser(user: User, done: (err, user: User) => void) {
+    done(null, user);
+  }
+  async deserializeUser(user: User, done: (err, user: User) => void) {
+    const userDB = await this.userService.findUsersById(user.id);
+    return userDB ? done(null, userDB) : done(null, null);
+  }
 }
